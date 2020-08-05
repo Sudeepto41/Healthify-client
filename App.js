abe3b0192f2da06f97abc21fe89485f0cfbf38b0
Sudeepto41 <sudeeptosaha.412@gmail.com>
2020-08-06 03:55:10 +0530

    m

--- App.js
+++ App.js
@@ -3,36 +3,37 @@ import { StyleSheet, Text, View, _View } from "react-native";
 import * as Fonts from "expo-font";
 import { AppLoading } from "expo";
 
-import Intro from "./app/screens/OTP";
+import Intro from "./app/screens/intro";
 
 const getFonts = () => {
-  return Fonts.loadAsync({
-    "nunito-ExtraLight": require("./app/assets/fonts/NunitoSans-ExtraLight.ttf"),
-    "nunito-Light": require("./app/assets/fonts/NunitoSans-Light.ttf"),
-    "nunito-regular": require("./app/assets/fonts/NunitoSans-Regular.ttf"),
-    "nunito-SemiBold": require("./app/assets/fonts/NunitoSans-SemiBold.ttf"),
-    "nunito-Bold": require("./app/assets/fonts/NunitoSans-Bold.ttf"),
-    "nunito-ExtraBold": require("./app/assets/fonts/NunitoSans-ExtraBold.ttf"),
-    "nunito-Black": require("./app/assets/fonts/NunitoSans-Black.ttf"),
-  });
+    return Fonts.loadAsync({
+        "nunito-ExtraLight": require("./app/assets/fonts/NunitoSans-ExtraLight.ttf"),
+        "nunito-Light": require("./app/assets/fonts/NunitoSans-Light.ttf"),
+        "nunito-regular": require("./app/assets/fonts/NunitoSans-Regular.ttf"),
+        "nunito-SemiBold": require("./app/assets/fonts/NunitoSans-SemiBold.ttf"),
+        "nunito-Bold": require("./app/assets/fonts/NunitoSans-Bold.ttf"),
+        "nunito-ExtraBold": require("./app/assets/fonts/NunitoSans-ExtraBold.ttf"),
+        "nunito-Black": require("./app/assets/fonts/NunitoSans-Black.ttf"),
+    });
 };
 export default function App() {
-  const [fontsLoaded, setfontsLoaded] = useState(false);
+    const [fontsLoaded, setfontsLoaded] = useState(false);
 
-  if (fontsLoaded) {
-    return <Intro />;
-  } else {
-    return (
-      <AppLoading startAsync={getFonts} onFinish={() => setfontsLoaded(true)} />
-    );
-  }
+    if (fontsLoaded) {
+        return <Intro />;
+    }
+    else {
+        return (
+            <AppLoading startAsync={getFonts} onFinish={() => setfontsLoaded(true)} />
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
--- app/screens/intro.js
+++ app/screens/intro.js
@@ -1,63 +1,64 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  ImageBackground,
-  Dimensions,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    ImageBackground,
+    Dimensions,
+    TouchableWithoutFeedback,
 } from "react-native";
 
 import color from "../config/color";
 
 export default function Intro() {
-  return (
-    <View style={styles.container}>
-      <ImageBackground
-        style={styles.BackgroundImage}
-        source={require("../assets/image/intro_image.png")}
-      >
-        <Text style={styles.Text}>
-          Hi there!{"\n"}
-          We'll be taking care of ur{"\n"}
-          health from now on.
-        </Text>
-
-        <Image
-          source={require("../assets/image/nextButton.png")}
-          style={styles.ArrowButton}
-        ></Image>
-      </ImageBackground>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Image
+                style={styles.BackgroundImage}
+                source={require("../assets/image/intro_image.png")}
+            ></Image>
+            <Text style={styles.Text}>
+                Hi there!{"\n"}
+                We'll be taking care of ur{"\n"}
+                health from now on.
+            </Text>
+            <TouchableWithoutFeedback onPress={() => alert('TU CHUTIYA LAVDE!')}>
+                <Image
+                    source={require("../assets/image/nextButton.png")}
+                    style={styles.ArrowButton}>
+                </Image>
+            </TouchableWithoutFeedback>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: color.bg_light,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  BackgroundImage: {
-    alignItems: "center",
-    position: "absolute",
-    top: Dimensions.get("window").height / 5, //margin TOP
-    height: Dimensions.get("window").height / 2.5, //HEIGHT
-    width: Dimensions.get("window").width, //WIDTH
-  },
-  Text: {
-    position: "absolute",
-    top: Dimensions.get("window").height / 1.5,
-    fontFamily: "nunito-Black",
-    color: "#4B5769",
-    fontSize: 20,
-    textAlign: "center",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: color.bg_light,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    BackgroundImage: {
+        alignItems: "center",
+        position: "absolute",
+        top: Dimensions.get("window").height / 5, //margin TOP
+        height: Dimensions.get("window").height / 2.5, //HEIGHT
+        width: Dimensions.get("window").width, //WIDTH
+    },
+    Text: {
+        position: "absolute",
+        top: Dimensions.get("window").height / 1.5,
+        fontFamily: "nunito-Black",
+        color: "#4B5769",
+        fontSize: 20,
+        textAlign: "center",
+    },
 
-  ArrowButton: {
-    position: "absolute",
-    top: Dimensions.get("window").height / 1.2,
-  },
+    ArrowButton: {
+        position: "absolute",
+        top: Dimensions.get("window").height / 1.2,
+    },
 });
