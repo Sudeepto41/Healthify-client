421a28b077e3ec427c4eb06492c506c9711c7a50
Sudeepto41 <sudeeptosaha.412@gmail.com>
2020-08-06 03:36:48 +0530

    intro splash screen

--- app/screens/intro.js
+++ app/screens/intro.js
@@ -1,23 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, ImageBackground, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground, Dimensions, TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
 
 import color from '../config/color'
 
+
 export default function Intro() {
     return (
-        <View style={styles.container}>
-            <ImageBackground style={styles.BackgroundImage}
-                source={require('../assets/image/intro_image.png')}>
 
-                <Text style={styles.Text}>
-                    Hi there!{"\n"}
+        <View style={styles.container}>
+            <Image style={styles.BackgroundImage}
+                source={require('../assets/image/intro_image.png')}>
+            </Image>
+            <Text style={styles.Text}>
+                Hi there!{"\n"}
                     We'll be taking care of ur{"\n"}
                     health from now on.
                 </Text>
-
+            <TouchableWithoutFeedback onPress={() => console.log('saxx')} style={{ alignItems: 'center' }}>
                 <Image source={require('../assets/image/nextButton.png')} style={styles.ArrowButton}></Image>
-            </ImageBackground>
+            </TouchableWithoutFeedback>
+
         </View >
     );
 }
@@ -30,23 +33,24 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+    BackgroundImage: {
+        alignItems: 'center',
+        position: 'absolute',
+        top: Dimensions.get('window').height / 5,     //margin TOP
+        height: Dimensions.get('window').height / 2.5,  //HEIGHT
+        width: Dimensions.get('window').width           //WIDTH
+    },
     Text: {
         position: 'absolute',
-        top: Dimensions.get('window').height / 2,
+        top: Dimensions.get('window').height / 1.5,
         fontFamily: 'nunito-Black',
         color: '#4B5769',
         fontSize: 20,
         textAlign: 'center',
     },
-    BackgroundImage: {
-        alignItems: 'center',
-        position: 'absolute',
-        top: Dimensions.get('window').height / 6.3,     //margin TOP
-        height: Dimensions.get('window').height / 2.5,  //HEIGHT
-        width: Dimensions.get('window').width           //WIDTH
-    },
+
     ArrowButton: {
         position: 'absolute',
-        top: Dimensions.get('window').height / 1.5,
-    }
+        top: Dimensions.get('window').height / 1.2,
+    },
 });
