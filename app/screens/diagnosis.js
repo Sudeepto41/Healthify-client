1680100bf0cb6da60e82ba9e12710e0760520458
vithikaa <vithikajha14@gmail.com>
2020-10-06 22:25:14 +0530

    test diagnosis page

---
+++ app/screens/diagnosis.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+  Image,
+  Dimensions,
+  TextInput,
+} from "react-native";
+
+export default function login({ navigation }) {
+  return (
+    <View>
+      <Text>vithika</Text>
+    </View>
+  );
+}
